cc24243ffc1b35ffc87a39085a5a4d79f99d0c1a
jafet1asanchez <jafet1asanchez@gmail.com>
2025-05-19 16:13:10 -0600

    Ejercicio 2 B
    Reescribimos la funcion pero esta vez en una Arrow Function y probamos que funcione

--- ejercicios.js
+++ ejercicios.js
@@ -1,3 +1,5 @@
+// EJERCICIO 1
+
 //Con el let declaramos una variable que puede cambiar más adelante
 let nombre = "Armando";
 
@@ -12,3 +14,15 @@ const saludo = "Hola, " + nombre + ". Tienes " + edad + " años."
 
 //Mostramos el resultado en consola
 console.log(saludo);
+
+// EJERCICIO 2
+
+//Escribimos una funcion tipo flecha (Arrow function) para que calcule el cuadrado de un numero
+const cuadrado = (numero) => numero * numero;
+
+//Pruebas con 3 numeros
+console.log(cuadrado(2));
+console.log(cuadrado(8));
+console.log(cuadrado(10));
+
+
