87df00d503fd2757e95d4c64f45481d31eddca4d
jafet1asanchez <jafet1asanchez@gmail.com>
2025-05-19 16:05:48 -0600

    Primer ejercicio A
    Cambiamos variables de var por let y const para seguir las buenas practicas modernas de Js

---
+++ ejercicios.js
@@ -0,0 +1,14 @@
+//Con el let declaramos una variable que puede cambiar más adelante
+let nombre = "Armando";
+
+//Se sigue manteniendo como constante porque la edad no cambia
+const edad = 25;
+
+//Reasignamos el valor de nombre
+nombre = "Jafet Abraham"
+
+//Saludo
+const saludo = "Hola, " + nombre + ". Tienes " + edad + " años."
+
+//Mostramos el resultado en consola
+console.log(saludo);
