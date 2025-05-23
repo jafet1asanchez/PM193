ecb3f0b3f4a5ac0e3ea6c33bb546dd64f5addff1
jafet1asanchez <jafet1asanchez@gmail.com>
2025-05-23 14:38:23 -0600

    Practica 3 Ejercicio A
    Creamos una funcion para restar 2 numeros y la exportarmos e importamos de un archivo a otro.

---
+++ Practica 3/Ejercicio A/main.js
@@ -0,0 +1,5 @@
+//Importa la funcion restar desde utils.js
+const restar = require('./utils');
+
+//Imprimimos el resultado dandole los valores para la resta
+console.log(restar(10, 5));
---
+++ Practica 3/Ejercicio A/utils.js
@@ -0,0 +1,7 @@
+//Funcion para restar 2 numeros
+function restar(a, b) {
+    return a - b;
+}
+
+//Exportamos la función para poder usarla en el main.js
+module.exports = restar;
