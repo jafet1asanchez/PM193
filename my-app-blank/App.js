107b037d92ece57e83b9b4d9b27aa030876f2b85
jafet1asanchez <jafet1asanchez@gmail.com>
2025-06-12 16:07:28 -0600

    Commit inicial
    Descripción de las zonas y uso de propiedades y states

--- my-app-blank/App.js
+++ my-app-blank/App.js
@@ -1,15 +1,33 @@
+// ZONA A: Importaciones necesarias
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
+// Creamos un componente de texto que se repetirá
+// Agregammos el parametro props para recibir propiedades
+const Texto = (props) => {
+  // Desestructuramos el contenido de las propiedades
+  const { contenido } = props;
+  return (
+    // Mostramos el contenido recibido como propiedad
+      <Text> { contenido } </Text>
+  )
+}
+
+// ZONA B: Componente principal de la aplicación (MAIN)
 export default function App() {
   return (
+  //Llamamos al componente de texto varias veces
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Texto contenido = 'Hola'></Texto>
+      <Texto contenido = 'Mundo'></Texto>
+      <Texto contenido = 'React Native'></Texto>
+      <Button title='Presionar'></Button>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+// ZONA C: Estilos de la aplicación 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
